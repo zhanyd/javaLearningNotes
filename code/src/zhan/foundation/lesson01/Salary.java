86432ddc6b7dd1c9301572c44c5f1c98fca8161a
zhanyd <zhanyd@sina.com>
2018-02-02 09:48:50 +0800

    v1.0

---
+++ code/src/zhan/foundation/lesson01/Salary.java
@@ -0,0 +1,33 @@
+package zhan.foundation.lesson01;
+
+public class Salary implements Comparable<Salary>{
+
+	private String name;
+	private int baseSalary;
+	private int bonus;
+	
+	public Salary(String name,int baseSalary,int bonus){
+		this.name = name;
+		this.baseSalary = baseSalary;
+		this.bonus = bonus;
+	}
+	
+	public String toString(){
+		return "name = " + name + " （baseSalary*13+bonus）=" + (baseSalary*13 + bonus);
+	}
+	
+	@Override
+	public int compareTo(Salary o){
+		
+		if(this.baseSalary*13 + this.bonus < o.baseSalary*13 + o.bonus){
+			return 1;
+		}
+		if(this.baseSalary*13 + this.bonus == o.baseSalary*13 + o.bonus){
+			return 0;
+		}
+		else{
+			return -1;
+		}
+
+	}
+}
---
+++ doc/foundation/lesson01.md
@@ -0,0 +1,6 @@
+#1题
+ byte ba=127;
+ byte bb=ba<<2;
+ System.out.println(bb);
+这个为什么会出错？给出解释，并且纠正错误
+
